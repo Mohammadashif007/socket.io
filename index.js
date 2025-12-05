95978c39dd4ffb98dce266ccb103b21f8bf77e27
Mohammad Ashif <mohammadashif510@gmail.com>
2025-12-05 18:30:39 +0600

    express basic setup done

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello server");
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on server ${port}`);
+});
