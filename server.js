042c025a2cc0a8717b47364d9ac893c22e1696fa
Mohammad Ashif <mohammadashif510@gmail.com>
2025-12-11 14:12:19 +0600

    server setup done

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
 import express from "express";
-import http from "http";
 import { Server } from "socket.io";
 
 const app = express();
-const server = http.createServer(app);
+const port = 7000;
 
-// !initialized socket.io and attached to the http server
-const io = new Server(server);
-app.use(express.static("public"));
-
-const users = new Set();
-
-io.on("connection", (socket) => {
-    console.log("A new user connected");
-    socket.on("join", (userName) => {
-        users.add(userName);
-
-        // ! broadcast to all user that a new user has joined
-        socket.emit("userJoined", userName);
-
-        // ! send the updated user lint to all client
-        socket.emit("userList", Array.from(users));
-    });
+app.get("/", (req, res) => {
+    res.send("<h1>Hello world</h1>");
 });
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`App is running on ${port}`);
 });
