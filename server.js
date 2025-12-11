d5060e4b87c987d93c79222447d4166a626f721a
Mohammad Ashif <mohammadashif510@gmail.com>
2025-12-11 14:48:49 +0600

    user connected successfully

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ import http from "http";
 
 const app = express();
 const port = 7000;
-
 const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile("./public/index.html");
+});
+
+io.on("connect", (socket) => {
+    console.log("A user connected");
 });
 
 server.listen(port, () => {
